1b63f4a2ae7b28619916399446a8b300fa9e8d33
Mattia <60852205+DeusAres@users.noreply.github.com>
2022-03-08 21:57:04 +0100

    Add files via upload

--- speedItUp.py
+++ speedItUp.py
@@ -1,5 +1,8 @@
 from PIL import Image
 
+import time
+start_time = time.time()
+
 class listCheck():
     def __init__(self):
         self.entries = []
@@ -7,13 +10,18 @@ class listCheck():
         self.entries.append(check(*args))
     def checking(self, x,y):
         toRemove = []
+
         for i in range(len(self.entries)):
             result = self.entries[i].inside(x,y)
             if type(result) == int:
                 return result
             elif result == 'remove':
                 toRemove.append(i)
-            
+
+        if len(toRemove) != 0:
+            self.entries = [self.entries[i] for i in range(len(self.entries)) if i not in toRemove]
+
+
 class check():
     def __init__(self, x1, y1, x2, y2):
         self.x1 = x1
@@ -22,14 +30,14 @@ class check():
         self.y2 = y2
 
     def inside(self, x, y):
-        if x >= self.x1 and x<= self.x2:
-            if y >= self.y1 and y <= self.y2:
+        if x >= self.x1 and x < self.x2:
+            if y >= self.y1 and y < self.y2:
                 return self.y2-self.y1
         elif x > self.x2:
             return 'remove'
 
 if __name__ == '__main__':
-    template = "test.png"
+    template = "./test.png"
     template = Image.open(template)
     W, H = template.size
     template = template.load()
@@ -37,21 +45,32 @@ if __name__ == '__main__':
     coordinates = []
     checkList = listCheck()
 
-    for x in range(W):
-        for y in range(H):
+    x = 0
+    while x < W:
+        y = 0
+        while y < H:
+            if x == 177 and y == 72: 
+                print('found')
+
             result = checkList.checking(x,y)
             if type(result) == int:
-                y += result-1
+                y += result
                 continue
 
-            if template[x,y] == (0,0,0):
+            if template[x,y] != (255, 255, 255):
                 y1 = y
-                while y1<H and template[x,y1] == (0,0,0) :
+                while template[x,y1] != (255, 255, 255):
                     y1 += 1
+                    if y1>H : break
                 x1 = x
-                while x1<W and template[x1,y] == (0,0,0) :
+                while template[x1,y] != (255, 255, 255):
                     x1 += 1
+                    if x1>W : break
 
-                coordinates.append([x,y,x1-1,y1-1])
+                coordinates.append([x,y,x1,y1])
 
-                checkList.append([x,y,x1-1,y1-1])
+                checkList.append([x,y,x1,y1])
+            y+=1
+        x+=1
+
+    print("--- %s seconds ---" % (time.time() - start_time))
